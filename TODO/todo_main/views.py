834ac7e1c20e6fbfa6c294ebfb790e7fa6f9e35d
bhagyashree <bhagyashree.kanamadi@sekel.tech>
2025-06-21 20:28:53 +0530

    added

--- TODO/todo_main/views.py
+++ TODO/todo_main/views.py
@@ -2,15 +2,22 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
+from todo.models import Task
+
 
 def home(request):
     # return HttpResponse('<h2>Homepage</h2>')
-    return render(request,'home.html')
+    tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+    # print(tasks)
+    context={
+        'tasks':tasks,
+    }
+    return render(request,'home.html',context)
 
-def add_task(request):
-    if request.method == "POST":
-        task_text = request.POST.get("task")
-        if task_text:
-            # Save to your model (e.g., Task.objects.create(task=task_text))
-            pass
-    return redirect('home')
+# def add_task(request):
+#     if request.method == "POST":
+#         task_text = request.POST.get("task")
+#         if task_text:
+#             # Save to your model (e.g., Task.objects.create(task=task_text))
+#             Task.objects.create(task=task_text)
+#     return redirect('home')
